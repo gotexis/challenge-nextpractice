8b79e6ca8f665bbe2f42cda0c9767c9c4b28f412
Exis Zhang <gotexis@gmail.com>
2020-01-31 20:43:11 +1100

    1 hour mark

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {DynamicForm} from "./components/Form";
+import Container from "@material-ui/core/Container";
 
-function App() {
+const formSchema = [
+  'name',
+  'dob',
+  'gender',
+  'contact',
+  'requireGuardianConsent',
+  'guardianDetails',
+]
+
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Container maxWidth="sm" style={{padding: '10% 0'}}>
+          <DynamicForm schema={formSchema}/>
+        </Container>
+      </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,233 @@
+import React, {Fragment} from "react";
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import update from 'immutability-helper';
+import {Select} from "@material-ui/core";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import {format} from 'date-fns'
+
+const contactOptions = ['mobile', 'home']
+
+export class DynamicForm extends React.Component {
+
+  state = {
+    name: '',
+    dob: this.getMaxDate(),
+    gender: '',
+    contact: [
+      {
+        type: '',
+        value: ''
+      }
+    ],
+    requireGuardianConsent: false,
+    guardian: [
+      {
+        name: '',
+        contact: '',
+      }
+    ],
+    $internal: {
+      msgOpen: false,
+      msg: '',
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.handleDeepChange = this.handleDeepChange.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleDateChange = this.handleDateChange.bind(this)
+    this.addContactLine = this.addContactLine.bind(this)
+    this.addGuardianLine = this.addGuardianLine.bind(this)
+    this.submit = this.submit.bind(this)
+  }
+
+  /*
+  * category: contact / guardian
+  * objectKey: type / value / name / contact
+  */
+  handleDeepChange(event, i, objectKey, category) {
+    const newVal = event.target.value
+    const newState = update(this.state, {
+      [category]: {
+        [i]: {
+          [objectKey]: {$set: newVal}
+        }
+      }
+    });
+    this.setState(newState);
+  }
+
+  handleChange(event, field) {
+    const newVal = event.target.value
+    this.setState({
+      [field]: newVal
+    })
+  }
+
+  handleDateChange(event, field) {
+    this.setState({
+      [field]: event
+    })
+  }
+
+  handleCheck(event, field) {
+    const newVal = event.target.checked
+    this.setState({
+      [field]: newVal
+    })
+  }
+
+  addContactLine() {
+    this.setState({
+      contact: [...this.state.contact, {
+        type: '',
+        value: ''
+      }]
+    })
+  }
+
+  addGuardianLine() {
+    this.setState({
+      guardian: [...this.state.guardian, {
+        name: '',
+        contact: '',
+      }]
+    })
+  }
+
+  getMaxDate() {
+    const d = new Date()
+    d.setFullYear(d.getFullYear() - 18);
+    return d
+  }
+
+  submit(e) {
+    e.preventDefault()
+
+    // prepare data
+    const data = Object.fromEntries(
+        Object.entries(this.state)
+            .filter(([k, v]) => !['requireGuardianConsent', '$internal'].includes(k))
+    )
+
+    if (Object.keys(data).includes('dob')) {
+      data.dob = format(data.dob, 'yyyy-MM-dd')
+    }
+
+    // validation
+    let msg
+    const nameValidator = (v) => v.trim().includes(" ")
+    if (this.props.schema.includes('name') && !nameValidator(data.name)) {
+      msg = 'Name validation failed! (Need to contain at least 2 words)'
+    } else {
+      msg = 'Successfully submitted! Check console.'
+    }
+
+    // notification
+    const newState = update(this.state.$internal, {
+      msgOpen: {$set: true},
+      msg: {$set: msg},
+    });
+    this.setState(newState);
+
+    console.log(data);
+  }
+
+  render() {
+    let {schema} = this.props;
+
+    // schema // name, dob, gender, contact, require guardian consent, guardianDetails
+
+    return <>
+      <form onSubmit={this.submit}>
+        <FormControl>
+          {
+            this.props.schema.includes('name') &&
+            <TextField label='name' required onChange={e => this.handleChange(e, 'name')}/>
+          }
+          {/* put date picker here */}
+          {this.props.schema.includes('gender') && <FormControl>
+            <InputLabel>gender</InputLabel>
+            <Select
+                onChange={(event) =>
+                    this.handleChange(event, 'gender')}>
+              <MenuItem value='Male'>
+                Male
+              </MenuItem>
+              <MenuItem value='Female'>
+                Female
+              </MenuItem>
+              <MenuItem value='Others'>
+                Others
+              </MenuItem>
+            </Select>
+          </FormControl>}
+          {this.props.schema.includes('contact') && <>
+            <h3>Contact details</h3>
+            <Button variant="contained" onClick={this.addContactLine}>
+              Add
+            </Button>
+            {this.state.contact.map((field, i) =>
+                <>
+                  <FormControl>
+                    <InputLabel>type</InputLabel>
+                    <Select onChange={
+                      (event) =>
+                          this.handleDeepChange(event, i, 'type', 'contact')
+                    }
+                            value={this.state.contact[i].type}
+                    >
+                      {contactOptions.map(option => (
+                          <MenuItem key={option} value={option}>
+                            {option}
+
+                          </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <TextField label='value' onChange={event => this.handleDeepChange(event, i, 'value', 'contact')}/>
+                </>
+            )}
+          </>}
+          {this.props.schema.includes('requireGuardianConsent') && <FormControlLabel
+              control={
+                <Checkbox
+                    checked={this.state.requireGuardianConsent}
+                    onChange={(event) => this.handleCheck(event, 'requireGuardianConsent')}
+                    value="requireGuardianConsent"/>
+              }
+              label="Require Guardian Consent"
+          />}
+          {
+            this.props.schema.includes('guardianDetails') && this.state.requireGuardianConsent && <>
+              <h3>Guardian details</h3>
+              <Button variant="contained" onClick={this.addGuardianLine}>
+                Add
+              </Button>
+              {this.state.guardian.map((field, i) =>
+                  <Fragment key={i}>
+                    <TextField label='name'
+                               onChange={event => this.handleDeepChange(event, i, 'name', 'guardian')}
+                    />
+                    <TextField label='contact'
+                               onChange={event => this.handleDeepChange(event, i, 'contact', 'guardian')}
+                    />
+                  </Fragment>
+              )}
+            </>
+          }
+          <br/>
+          <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button>
+        </FormControl>
+      </form>
+    </>
+  }
+}
